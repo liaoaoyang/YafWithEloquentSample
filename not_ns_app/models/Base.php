accbc75a2576ebcfc410a2d42c7879f61689ae62
liaoaoyang <liaoaoyang@gmail.com>
2018-05-03 00:20:49 +0800

    Ensure every model use the same capsule

--- not_ns_app/models/Base.php
+++ not_ns_app/models/Base.php
@@ -6,22 +6,44 @@ use Illuminate\Database\Eloquent\Model as IlluminateModel;
 
 class BaseModel extends IlluminateModel
 {
-    protected $config = null;
-    protected $capsule = null;
+    protected        $config   = null;
+    protected        $capsule  = null;
+    protected static $capsules = [];
 
     public function __construct(array $attributes = array())
     {
         parent::__construct($attributes);
         $dbConfigKey = DATABASE_CONFIG_KEY;
-        $this->config = Yaf_Registry::get('config');
+        $this->config = YRegistry::get('config');
 
         if (!$this->config->$dbConfigKey) {
             throw new Exception("Must configure database in .ini first");
         }
 
         $this->config = $this->config->$dbConfigKey->toArray();
-        $this->capsule = new IlluminateCapsule();
-        $this->capsule->addConnection($this->config);
-        $this->capsule->bootEloquent();
+        $entry = self::getCapsuleEntry($this->config);
+
+        if (isset(self::$capsules[$entry])) {
+            $capsule = self::$capsules[$entry];
+        } else {
+            $capsule = new IlluminateCapsule();
+            $capsule->addConnection($this->config);
+            $capsule->bootEloquent();
+            self::$capsules[$entry] = $capsule;
+        }
+
+        $this->capsule = $capsule;
+    }
+
+    protected static function getCapsuleEntry($config)
+    {
+        if (!is_array($config)) {
+            return '_';
+        }
+
+        $configValues = array_values($config);
+        sort($configValues);
+
+        return md5(join('_', $configValues));
     }
 }
